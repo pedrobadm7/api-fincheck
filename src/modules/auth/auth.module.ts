d1f70b08b57c69fa16af896305afe81010453407
pedrobadm7 <pedrobars7a@gmail.com>
2023-09-19 11:55:02 -0300

    feature: make AuthGuard global

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { UsersModule } from '../users/users.module';
 import { JwtModule } from '@nestjs/jwt';
 import { env } from 'src/shared/config/env';
 
 @Module({
   imports: [
-    UsersModule,
     JwtModule.register({
+      global: true,
       signOptions: { expiresIn: '7d' },
       secret: env.jwtSecret,
     }),
--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,13 +1,11 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { AuthGuard } from '../auth/auth.guard';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get('/me')
-  @UseGuards(AuthGuard)
   me(@Req() request: any) {
     console.log({ meuserId: request.userId });
     return this.usersService.getUserById('userId');
